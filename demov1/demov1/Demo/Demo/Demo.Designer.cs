d7c3847a89b90093cfd81c9b93e8c102c2c5ed86
Nguyen Hoang Thi <nguyenhoangthikh2004@gmail.com>
2025-02-08 19:06:33 +0700

    Demo Linq to EF

---
+++ demov1/demov1/Demo/Demo/Demo.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Demo
+{
+    partial class frmDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.grbList = new System.Windows.Forms.GroupBox();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cboCategory = new System.Windows.Forms.ComboBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.grbList.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(33, 30);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(78, 20);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Tên sách:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(30, 78);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(38, 20);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "Giá:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(117, 27);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(358, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(74, 75);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 26);
+            this.txtPrice.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(513, 23);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(101, 34);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(633, 23);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(101, 34);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(513, 71);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(101, 34);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // grbList
+            // 
+            this.grbList.Controls.Add(this.dgvBooks);
+            this.grbList.Location = new System.Drawing.Point(32, 133);
+            this.grbList.Name = "grbList";
+            this.grbList.Size = new System.Drawing.Size(702, 299);
+            this.grbList.TabIndex = 4;
+            this.grbList.TabStop = false;
+            this.grbList.Text = "Danh sách:";
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvBooks.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvBooks.Location = new System.Drawing.Point(3, 22);
+            this.dgvBooks.MultiSelect = false;
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.ReadOnly = true;
+            this.dgvBooks.RowHeadersWidth = 62;
+            this.dgvBooks.RowTemplate.Height = 28;
+            this.dgvBooks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvBooks.Size = new System.Drawing.Size(696, 274);
+            this.dgvBooks.TabIndex = 0;
+            this.dgvBooks.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBooks_CellContentClick);
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(179, 78);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(68, 20);
+            this.lblCategory.TabIndex = 6;
+            this.lblCategory.Text = "Thể loại:";
+            // 
+            // cboCategory
+            // 
+            this.cboCategory.FormattingEnabled = true;
+            this.cboCategory.Location = new System.Drawing.Point(263, 75);
+            this.cboCategory.Name = "cboCategory";
+            this.cboCategory.Size = new System.Drawing.Size(212, 28);
+            this.cboCategory.TabIndex = 7;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(633, 71);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(101, 34);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Thoát";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 454);
+            this.Controls.Add(this.cboCategory);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.grbList);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmDemo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmDemo_FormClosing);
+            this.grbList.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.GroupBox grbList;
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cboCategory;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ demov1/demov1/Demo/Demo/Demo.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class frmDemo : Form
+    {
+        QLSACHEntities db = new QLSACHEntities();
+        public frmDemo()
+        {
+            InitializeComponent();
+           
+            this.LoadData();
+            this.LoadCategory();
+            this.cboCategory.SelectedIndex = -1;
+            //this.AddBinding();
+        }
+
+        #region Methods
+        //void AddBinding()
+        //{
+        //    this.txtName.DataBindings.Add(new Binding("Text", this.dgvBooks.DataSource, "Name", true, DataSourceUpdateMode.Never));
+        //    this.txtPrice.DataBindings.Add(new Binding("Text", this.dgvBooks.DataSource, "Price", true, DataSourceUpdateMode.Never));
+        //    this.cboCategory.DataBindings.Add(new Binding("Text", this.dgvBooks.DataSource, "Category", true, DataSourceUpdateMode.Never));
+        //}
+
+        bool CheckInput()
+        {
+            if (string.IsNullOrEmpty(this.txtName.Text))
+                return false;
+
+            if (string.IsNullOrEmpty(this.txtPrice.Text))
+                return false;
+
+            if (this.cboCategory.SelectedIndex == -1)
+                return false;
+
+            return true;
+        }
+
+        bool CheckUniqueBook()
+        {
+            // Cách 1:
+            //BOOKS book = db.BOOKS.Where(p => p.NAME == this.txtName.Text).SingleOrDefault();
+            //if (book != null)
+            //    return false;
+
+            // Cách 2:
+            var result  = from b in db.BOOKS
+                          where b.NAME == this.txtName.Text
+                          select b;
+            if(result.Count() > 0)
+                return false;
+
+            return true;
+        }
+
+        void LoadData()
+        {
+            // select tất cả dữ liệu từ bảng BOOKS
+            // Cách 1:
+            // var reuslt = from book in db.BOOKS select book;
+            // this.dgvBooks.DataSource = result.ToList();
+
+            // Cách 2:
+            // this.dgvBooks.DataSource = db.BOOKS.ToList();
+
+            // select cột tùy ý
+            var result = from book in db.BOOKS
+                         select new
+                         {
+                             ID = book.ID,
+                             Name = book.NAME,
+                             Price = book.PRICE,
+                             Category = book.CATEGORY.NAME
+                         };
+            this.dgvBooks.DataSource = result.ToList();
+            this.dgvBooks.Columns[0].Visible = false;
+            this.dgvBooks.Columns[1].HeaderText = "Tên sách";
+            this.dgvBooks.Columns[2].HeaderText = "Đơn giá";
+            this.dgvBooks.Columns[3].HeaderText = "Thể loại";
+        }
+
+        void LoadCategory()
+        {
+            this.cboCategory.DataSource = db.CATEGORY.ToList();
+            this.cboCategory.DisplayMember = "NAME";
+            this.cboCategory.ValueMember = "ID";
+        }
+
+        void AddBook()
+        {
+            if (!this.CheckInput() || !this.CheckUniqueBook())
+            {
+                MessageBox.Show("Dữ liệu không hợp lệ");
+                return;
+            }
+
+            BOOKS book = new BOOKS();
+            book.NAME = this.txtName.Text;
+            book.PRICE = float.Parse(this.txtPrice.Text);
+            book.ID_CATEGORY = (int)this.cboCategory.SelectedValue;
+
+            db.BOOKS.Add(book);
+            db.SaveChanges();
+        }
+
+        void DeleteBook()
+        {
+            int id1 = int.Parse(this.dgvBooks.SelectedRows[0].Cells["ID"].Value.ToString());
+            int id = int.Parse(this.dgvBooks.SelectedCells[0].OwningRow.Cells["ID"].Value.ToString());
+            BOOKS book = db.BOOKS.Find(id);
+            if (DialogResult.OK == MessageBox.Show("Bạn có chắc muốn xóa?", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Question))
+            {
+                db.BOOKS.Remove(book);
+                db.SaveChanges();
+                MessageBox.Show("Xóa thành công");
+            }
+        }
+
+        void UpdateBook()
+        {
+            int id = int.Parse(this.dgvBooks.SelectedCells[0].OwningRow.Cells["ID"].Value.ToString());
+
+            if(!this.CheckInput())
+            {
+                MessageBox.Show("Dữ liệu không hợp lệ");
+                return;
+            }
+
+            BOOKS book = db.BOOKS.Find(id);
+            book.NAME = this.txtName.Text;
+            book.PRICE = float.Parse(this.txtPrice.Text);
+            book.ID_CATEGORY = (int)this.cboCategory.SelectedValue;
+
+            db.SaveChanges();
+        }
+
+        #endregion
+
+        #region Events
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this.AddBook();
+            this.LoadData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            this.DeleteBook();
+            this.LoadData();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            this.UpdateBook();
+            this.LoadData();
+        }        
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmDemo_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(DialogResult.Yes == MessageBox.Show("Bạn có muốn thoát?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void dgvBooks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            this.txtName.Text = this.dgvBooks.SelectedRows[0].Cells["Name"].Value.ToString();
+            this.txtPrice.Text = this.dgvBooks.SelectedRows[0].Cells["Price"].Value.ToString();
+            this.cboCategory.Text = this.dgvBooks.SelectedRows[0].Cells["Category"].Value.ToString();
+        }
+        #endregion
+
+    }
+}
